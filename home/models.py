9e551c5e3f65c588a7a8b44947eb00c9e2fb4857
RahmatUllahSA <rahmatullah.iubat.cse@gmail.com>
2025-04-26 00:34:51 +0600

    change some code

--- home/models.py
+++ home/models.py
@@ -1,10 +1,21 @@
-from django.db import models  #2 templates
+from django.db import models
+from django.utils import timezone
 
-class Product(models.Model):   #2 templates
-    name = models.CharField(max_length=100)  #2 templates
-    price = models.DecimalField(max_digits=10, decimal_places=2)  #2 templates
-    description = models.TextField()   #2 templates
-    image = models.ImageField(upload_to='products/')  #2 templates
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
 
-    def __str__(self):  #2 templates
-        return self.name  #2 templates
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True, blank=True)  # <-- Add this
+    image = models.ImageField(upload_to='product_images/')
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    discount_price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    description = models.TextField()
+    created_at = models.DateTimeField(default=timezone.now)  # <-- Change this
+
+    def __str__(self):
+        return self.name
+
