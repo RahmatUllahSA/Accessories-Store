478870b3d0e40f0735b19b2ac94005f803c53938
RahmatUllahSA <rahmatullah.iubat.cse@gmail.com>
2025-04-25 23:39:44 +0600

    add some file and modify the code

--- home/urls.py
+++ home/urls.py
@@ -4,5 +4,6 @@ from django.urls import path  # Home app
 from . import views  # Home app
 
 urlpatterns = [
-    path('',views.home, name= 'home'),  # Home app
+    path('',views.home, name= 'home'),  #  1 Home app
+     path('category/<str:category_name>/', views.category_products, name='category_products'),  # 3 category
 ]
--- home/views.py
+++ home/views.py
@@ -8,3 +8,18 @@ def home(request):                      # 1 Home app
     #return HttpResponse('Home')         # 1 Home app
     products = Product.objects.all()  # 2 templates
     return render(request, 'home/home.html', {'products' : products})  # 2 templates
+
+def category_products(request, category_name):  # 3 category 
+    # ক্যাটাগরি অনুযায়ী প্রোডাক্ট টাইপ ফিল্টার করুন
+    products = {                                     # 3 category 
+        'earbuds': ['Earbuds 1', 'Earbuds 2', 'Earbuds 3'],      # 3 category 
+        'headphones': ['Headphone 1', 'Headphone 2', 'Headphone 3'],   # 3 category 
+        'airbuds': ['Airbud 1', 'Airbud 2', 'Airbud 3'],      # 3 category 
+        'smartwatch': ['Smart Watch 1', 'Smart Watch 2'],     # 3 category 
+        'pendrive': ['Pendrive 1', 'Pendrive 2'],               # 3 category 
+        'computerbox': ['Computer Box 1', 'Computer Box 2'],    # 3 category 
+        'mouse': ['Mouse 1', 'Mouse 2'],                        # 3 category 
+        'keyboard': ['Keyboard 1', 'Keyboard 2'],                # 3 category 
+    }
+    product_list = products.get(category_name, [])              # 3 category 
+    return render(request, 'home/category_products.html', {'category_name': category_name, 'product_list': product_list}) # 3 category 
